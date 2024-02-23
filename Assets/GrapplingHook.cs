b03ce2b0abac8720a8fddadaac0e47b9c8fa1da0
SteinsGate12345 <Ppx631129@gmail.com>
2024-02-22 19:05:26 -0500

    12
    1

--- Assets/GrapplingHook.cs
+++ Assets/GrapplingHook.cs
@@ -9,6 +9,8 @@ public class GrapplingHook : MonoBehaviour
     [SerializeField] private LineRenderer rope;
     private Vector3 grapplePoint;
     private DistanceJoint2D joint;
+    [SerializeField] private float pullSpeed;
+    [SerializeField] private float CloseDistance;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,15 +24,18 @@ public class GrapplingHook : MonoBehaviour
     void Update()
     {
         if(Input.GetMouseButtonDown(0))
-        {
-            RaycastHit2D hit = Physics2D.Raycast(
-                origin: Camera.main.ScreenToWorldPoint(Input.mousePosition),
-                direction: Vector2.zero,
-                distance: Mathf.Infinity,
-                layerMask: grappleLayer
-            );
+        {   
+            Vector3 mouse = Input.mousePosition;
+            Vector3 point = Camera.main.WorldToScreenPoint(transform.position);
+            Vector3 direction = mouse - point;
+            direction.z = 0f;
+            direction = direction.normalized;
+            transform.up = direction;
 
-            if( hit.collider != null ) {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position + new Vector3(0, 0.1f, 0), direction, grappleLength, grappleLayer);
+
+            if(hit.collider != null)
+            {
                 grapplePoint = hit.point;
                 grapplePoint.z = 0;
                 joint.connectedAnchor = grapplePoint;
@@ -40,6 +45,12 @@ public class GrapplingHook : MonoBehaviour
                 rope.SetPosition(1, transform.position);
                 rope.enabled = true;
             }
+
+        }
+
+        if (joint.enabled)
+        {
+        joint.distance = Mathf.Max(joint.distance - Time.deltaTime * pullSpeed, CloseDistance);
         }
 
         if(Input.GetMouseButtonUp(0))
