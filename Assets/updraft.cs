2d30db1c9266aa36cbfef656adfa341509b4e48d
SteinsGate12345 <Ppx631129@gmail.com>
2024-02-24 18:48:10 -0500

    气流方块

---
+++ Assets/updraft.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class updraft : MonoBehaviour
+{
+    public float upspeed = 0.1f;
+    // Start is called before the first frame update
+    void OnTriggerStay2D(Collider2D other)
+    {
+        GrapplingHook controller = other.GetComponent<GrapplingHook>();
+        var body = controller.GetComponent<Rigidbody2D>();
+        if (controller != null)
+        {
+            controller.transform.position += new Vector3(0,upspeed,0);
+            body.gravityScale = 0;
+        }
+    }
+
+     void OnTriggerExit2D(Collider2D other)
+    {
+        GrapplingHook controller = other.GetComponent<GrapplingHook>();
+        var body = controller.GetComponent<Rigidbody2D>();
+        if (controller != null)
+        {
+            body.gravityScale = 3;
+        }
+       
+    }
+
+}
