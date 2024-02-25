5a8519099b05577c3a3f702488b5d28040b96639
SteinsGate12345 <Ppx631129@gmail.com>
2024-02-24 19:20:19 -0500

    风方块

---
+++ Assets/corsswind.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class corsswind : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float crossSpeed = 0.1f;
+    // Start is called before the first frame update
+    void OnTriggerStay2D(Collider2D other)
+    {
+        GrapplingHook controller = other.GetComponent<GrapplingHook>();
+        var body = controller.GetComponent<Rigidbody2D>();
+        if (controller != null)
+        {
+            controller.transform.position += new Vector3(crossSpeed,0,0);
+            body.gravityScale = 0;
+        }
+    }
+
+     void OnTriggerExit2D(Collider2D other)
+    {
+        GrapplingHook controller = other.GetComponent<GrapplingHook>();
+        var body = controller.GetComponent<Rigidbody2D>();
+        if (controller != null)
+        {
+            body.gravityScale = 3;
+        }
+       
+    }
+}
+
