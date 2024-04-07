9cc52e2fef85c2c2718dd1caeec91563bc2f1028
SteinsGate12345 <Ppx631129@gmail.com>
2024-04-07 14:45:10 -0400

    更新

--- Assets/jumpPad.cs
+++ Assets/jumpPad.cs
@@ -4,20 +4,20 @@ using UnityEngine;
 
 public class jumpPad : MonoBehaviour
 {
-    private float jumpforce=400f;
+    private float jumpforce=1600f;
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.tag=="Player")
         {
             other.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up*jumpforce,ForceMode2D.Impulse);
             transform.position =Vector2.MoveTowards(transform.position, new Vector2(transform.position.x,
-             transform.position.y+3), 0.2f);
+             transform.position.y+7), 0.2f);
         }
     }
     private void OnCollisionExit2D(Collision2D other){
         if (other.gameObject.tag=="Player"){
             transform.position=Vector2.MoveTowards(transform.position,new Vector2(transform.position.x,
-             transform.position.y-3), 0.2f);
+             transform.position.y-7), 0.2f);
         }
     }
 }
